01f087bd613fc79156bce8b5a37a1ef3fb0c8175
PhiPrivate <anhphi178@gmail.com>
2023-10-23 17:23:31 +0200

    add key

--- app/blog/[postId]/comment.tsx
+++ app/blog/[postId]/comment.tsx
@@ -12,7 +12,7 @@ export default async function CommentDetails({postId}: CommentDetailProps ) {
             <h3>Comments:</h3>
             {comments.map((comment: any) => (
                 // eslint-disable-next-line react/jsx-key
-                <div>
+                <div key={postId}>
                     <h4>{comment.email}</h4>
                     <p>{comment.body}</p>
                 </div>
